c20a820dd9d15cc84068fa8e17044320589a56ab
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-03-29 22:30:01 +0500

    checek

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,7 +2,7 @@ from loader import dp, bot
 from aiogram import types,F
 import requests
 
-
+from aiogram.enums.chat_action import ChatAction
 from aiogram.types import  InputMediaPhoto, InputMediaVideo
 
 
@@ -22,12 +22,30 @@ async def echo_bot(message:types.Message):
                 await message.answer("Xatolik Yuz berdi Qayta urunib ko'ring")
                 return
         elif data["type"] == "stories":
-                media_group = []
-                for media in data["medias"]:
-                    media_group.append(InputMediaVideo(media=media["download_url"]))
-                await bot.send_chat_action(chat_id=message.chat.id, action="upload_video")
-                await message.answer_media_group(media_group)
-        await message.answer(message.text)
+            await bot.send_chat_action(chat_id=message.chat.id, action=ChatAction.UPLOAD_VIDEO)
+
+            photo_group = []
+            video_group = []  
+
+            for media in data["medias"]:
+                if media["type"] == "video":
+                    video_group.append(InputMediaVideo(media=media["download_url"], caption="Videos"))
+                else:
+                    photo_group.append(InputMediaPhoto(media=media["download_url"], caption="Photos"))
+
+                if len(video_group) == 10:
+                    await message.answer_media_group(video_group)
+                    video_group = []
+
+                if len(photo_group) == 10:
+                    await message.answer_media_group(photo_group)
+                    photo_group = []
+
+            if video_group:
+                await message.answer_media_group(video_group)
+
+            if photo_group:
+                await message.answer_media_group(photo_group)
     except Exception as e:
         print(e)
         await message.answer("Xatolik Yuz berdi Qayta urunib ko'ring")
