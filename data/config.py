63dd46c434a802c4813b122902d20ac9116c5842
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-04-12 09:21:02 +0500

    check

--- data/config.py
+++ data/config.py
@@ -2,5 +2,6 @@ from environs import Env
 env = Env()
 env.read_env()
 BOT_TOKEN=env.str('BOT_TOKEN')
+BOT_TOKEN="7784958688:AAEQyF-dqMCKRWraVFCDVlcfPQOnVo0uWwI"
 # BOT_TOKEN="7113658898:AAG3yuPySdtSkSB9HAuliS3rzwIs7HZ_pUs"
 ADMINS=env.list('ADMINS')
