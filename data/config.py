9f8626e06184d4d489f52cb6fa9154bdc21159b0
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-04-16 17:55:46 +0500

    check

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
 from environs import Env
 env = Env()
 env.read_env()
-# BOT_TOKEN=env.str('BOT_TOKEN')
-BOT_TOKEN="7113658898:AAGJmbRcFnoef7CRDIQRnnJ-yDGqd_s-9Cc"
+BOT_TOKEN=env.str('BOT_TOKEN')
+# BOT_TOKEN="7113658898:AAGJmbRcFnoef7CRDIQRnnJ-yDGqd_s-9Cc"
 ADMINS=env.list('ADMINS')
 GET_TOKEN_URL=env.str("GET_TOKEN_URL")
